359996d0c6b4c1d93d7994570d5594e7bdafd5d0
Nicholas koech <nicholaskoech@Nicholass-MacBook-Pro.local>
2022-01-07 03:36:49 -0800

    comment

--- src/Fibo.java
+++ src/Fibo.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Fibo {
+public class Fibo {
+    public static long fib(long n) {
+        //base condition
+        if ((n==0) || (n==1))
+            return n;
+        else
+            return fib(n-1)+ fib(n-2);
+    }
+    public static void main(String [] args) {
+        System.out.println("The 0th fibonacci number is:" +fib(0));
+        System.out.println("The 7th fibonacci number is:" +fib(7));
+        System.out.println("The 12th fibonacci number is:" +fib(12));
+    }
 }
